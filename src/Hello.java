cc469667df971d0f0d441cb5f12fb53e80319e96
Chris Z <eathena@hotmail.com>
2018-05-15 11:02:03 -0400

    added variables

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Chris!");
+        int myFirstNumber = 5 + 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int difference = 1000 - myTotal;
+        System.out.println(difference + " years");
     }
 }
+
+/* sout - keyboard shortcut to create -> System.out.println(); */
